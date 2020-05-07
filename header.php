da49b2a6da2896d917e609bdd393a91aa7e625bb
Gustavo <g.cflsouza@gmail.com>
2020-05-06 23:53:40 -0300

    um belo avanço
    Automatização no processo de minificação e estrutura de projeto
    
    Instalação do AOS Js para animação de componentes
    
    Alerta na HOME

--- header.php
+++ header.php
@@ -8,12 +8,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js" type="text/javascript"></script>
+    <link rel="stylesheet" href="<?php bloginfo('template_url') ?>/css/bootstrap.min.css">
 
+    <link rel="stylesheet" href="<?php bloginfo('template_url') ?>/css/aos.css" />
 
     <link rel="stylesheet" href="<?php bloginfo('template_url') ?>/style.css" />
+
+    <link rel="stylesheet" href="<?php bloginfo('template_url') ?>/css/main.min.css">
 
     <title>
       <?php if(is_front_page() || is_home()){
@@ -27,10 +28,19 @@
   </head>
   <body>
 
+
+  <div class="text-center alert alert-warning alert-dismissible show" role="alert">
+  <strong>COVID-19:</strong> You should check in on some of those fields below. <a href="#">Saiba mais</a>
+  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+    <span aria-hidden="true">&times;</span>
+  </button>
+</div>
+
+
 <header>
 
 <div class="container">
-  <nav class="navbar navbar-expand-lg navbar-light">
+  <nav class="navbar navbar-expand-lg navbar-dark">
     <a class="navbar-brand logo" href="<?php bloginfo('home') ?>"><img class="logo" src="<?php bloginfo('template_url') ?>/img/logo-dark.png" alt=""></a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
--- index.php
+++ index.php
@@ -1,26 +1,37 @@
 <?php get_header(); ?>
 
-<section class="d-flex align-items-center">
+<section class="d-flex align-items-center hero-home">
 	<div class="container">
+
+	<div class="row justify-content-md-center">
+	<div class="col-md-8">
+
+				<h1 data-aos="fade-down">Comunicação inteligente para o seu negócio</h1>
+				<p class="lead" data-aos="fade-up">Parecemos mais uma consultoria de gestão moderna que resolve grandes problemas para as empresas. Desde estratégia de marketing e experiência na Web até geração de leads - até transformação de cultura.</p>
+
+				<a class="btn-plus" href="#">Nosso trabalho</a>
+
+				<p class="partner"><small>Nós somos</small><br> Google Partner</p>
+		
+				</div>
+
+				</div>
+
+</div>
+</section>
+
+<section>
+	<div class="container">
+	<p>Soluções ideais para</p>
 		<div class="row">
-			<div class="col-md-6">
-				<h1>A transformative umgency pumrtner with creumtive spumark.</h1>
-				<p>Parecemos mais uma consultoria de gestão moderna que resolve grandes problemas para as empresas. Desde estratégia de marketing e experiência na Web até geração de leads - até transformação de cultura.</p>
-
-				<a href="#">Nosso trabalho</a>
+			<div class="col-md-3">
 			</div>
-
-			<div class="col-md-6">
-
-				<img src="https://s22474.pcdn.co/wp-content/uploads/2018/04/Mabbly-Round-Two-25_preview.jpeg" alt="">
-			</div>
-
 		</div>
 	</div>
 </section>
 
 
-<section>
+<section class="d-flex align-items-center">
 	<div class="container">
 		<div class="row">
 			<div class="col-md-6">
@@ -42,7 +53,7 @@
 
 
 
-<section>
+<section class="d-flex align-items-center">
 	<div class="container">
 		<div class="row">
 			<div class="col-md-6">
@@ -60,20 +71,26 @@
 </section>
 
 
-<section>
-	<div class="container">
+<section class="plans-home d-flex align-items-center">
+	<div class="container text-center">
 		<h1>Os resultados que a sua empresa merece</h1>
-		<p>É perfeitamente possível escalar um negócio utilizando estratégias digitais e nós podemos ajudar</p>
+		<p class="lead">É perfeitamente possível escalar um negócio utilizando estratégias digitais e nós podemos ajudar</p>
 
 		<div class="row">
 			<div class="col-md-4">
-				<ul>
-					<li>Outsourced CMO</li>
-					<li>Auditoria e Diagnóstico Empresarial</li>
-					<li>Análise Competitiva de Mercado</li>
-					<li>Planejamento Estretégico</li>
-					<li>Otimização de Conversões</li>
-				</ul>
+
+				<div class="plan-home">
+					<h3>Plano Team</h3>
+					<ul>
+						<li>Outsourced CMO</li>
+						<li>Auditoria e Diagnóstico Empresarial</li>
+						<li>Análise Competitiva de Mercado</li>
+						<li>Planejamento Estretégico</li>
+						<li>Otimização de Conversões</li>
+					</ul>
+					<a href="#">Consultar valores</a>
+				</div>
+
 			</div>
 			<div class="col-md-4">
 				<ul>
@@ -98,7 +115,7 @@
 </section>
 
 
-<section>
+<section class="d-flex align-items-center">
 	<div class="container">
 
 		<div class="title-section">
@@ -120,7 +137,7 @@
 </section>
 
 
-<section>
+<section class="d-flex align-items-center">
 	<div class="container">
 		<div class="row">
 
